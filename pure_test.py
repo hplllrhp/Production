4e17b0015934f178124bca8581f1aea6f1bb664b
hplllrhp <hplllrhp@outlook.com>
2018-03-18 18:57:28 +0800

    随手备份

--- pure_test.py
+++ pure_test.py
@@ -58,11 +58,11 @@ def get_flavor_property(inputFilePath):
 
 #print(server_property.property)
 #--------------------放置函数开始----------------------
-def JudgeRest(InputNum):
+def JudgeRest(InputNum,ToSerNum):
     flag = 0
     for i in range(InputNum):
         if(flavor_property.rest[i] != 0 and flavor_property.SingleVmFlag != 0):
-            if(server_property.cpu[0] >= flavor_property.cpu[i] and server_property.memory[0] >= flavor_property.memory[i]):
+            if(server_property.cpu[ToSerNum] >= flavor_property.cpu[i] and server_property.memory[ToSerNum] >= flavor_property.memory[i]):
                flag = 1
                break
     return flag
@@ -74,12 +74,10 @@ def SelectFlavorToSet(InputNum,i):
     for j in range(InputNum):
         if(flavor_property.rest[j] != 0 and flavor_property.SingleVmFlag != 0):
             a.append(j)
-    print('a = ',a)
     size = len(a)
     fn = a[0]
     for k in range(1,size):
         if(i == 0):
-            print('cpu',flavor_property.cpu[a[k]])
             if(flavor_property.cpu[a[k]] > flavor_property.cpu[fn]):
                 fn = a[k]
             elif(flavor_property.cpu[a[k]] < flavor_property.cpu[fn]):
@@ -89,7 +87,6 @@ def SelectFlavorToSet(InputNum,i):
                     fn = a[k]
                 else:
                     fn = fn
-            print('fn',fn)
         else:
             if(flavor_property.memory[a[k]] > flavor_property.memory[fn]):
                 fn = a[k]
@@ -117,25 +114,59 @@ def Judge(InputNum):
 #--------------------放置函数结束----------------------
 flavor_property = flavor_property()
 server_property = server_property()
+print('1',server_property.cpu,server_property.flavor,server_property.memory,server_property.ser_name)
+
 server_property = get_server_property(inputFilePath)
 flavor_property = get_flavor_property(inputFilePath)
+print('2',server_property.cpu,server_property.flavor,server_property.memory,server_property.ser_name)
+
+#server_property_static = get_server_property(inputFilePath)
+print('3',server_property.cpu,server_property.flavor,server_property.memory,server_property.ser_name)
+
 flavor_property.total = [2, 3, 3, 2, 6]#等于预测出来的数量之后用变量传递
 flavor_property.rest = [2, 3, 3, 2, 6]
-print('len',len(flavor_property.name))
-print(JudgeRest(5),SelectFlavorToSet(5,0),Judge(5)) 
 OptimizeFlag = 0
 server_property.ser_num = 0
 judge_flag = Judge(len(flavor_property.name))
 ToSerNum = 0;#//设置需要配置的服务器，ToSerNum = 0即第1个服务器
 fn = 0 #//当前放置优先级最高的虚拟机种类
+InputNum = len(flavor_property.name)
+print(server_property.cpu,server_property.flavor,server_property.memory,server_property.ser_name)        
+ 
 while (judge_flag):
     flag = 1#//设置能否配置的标志位，因为是还未配置的虚拟机，所以一开始是可以配置的
-    server_property.flavor[ToSerNum][:] = 0
-    
-list2 = [[0 for i in range(5)] for i in range(2)]
-list2[:][0] = 1
-  
+    server_property.flavor[ToSerNum][:] = [0 for i in range(5)]
+    while(flag):
+        fn = SelectFlavorToSet(InputNum,OptimizeFlag)
+        for i in range(flavor_property.rest[fn]):
+            if(flavor_property.cpu[fn] <= server_property.cpu[ToSerNum] and flavor_property.memory[fn] <= server_property.memory[ToSerNum]):
+                server_property.flavor[ToSerNum][fn] += 1
+                flavor_property.rest[fn] -= 1
+                server_property.cpu[ToSerNum] -= flavor_property.cpu[fn]
+                server_property.memory[ToSerNum] -= flavor_property.memory[fn]
+            else:
+                flavor_property.SingleVmFlag[fn] = 0#//标志该虚拟机在该服务器上不能再放置
+                break; #//内存没超，但服务器剩余的CPU数已经不能再放这种了虚拟机了，就跳出循环
+        #进入到下面这个if的情况：1)该虚拟机已经放置完  2)该虚拟机已不能再该台服务器上放置
+        if(JudgeRest(InputNum,ToSerNum) == 0):
+            flag = 0
+    if(Judge(InputNum) != 0):#判断一下，是不是所有虚拟机都配置完了，如果不是，那就新开一台物理服务器 
+        server_property.cpu.append(56)
+        server_property.memory.append(128)
+        server_property.ser_name.append('server')
+        server_property.flavor.append([0 for i in range(5)])
+        for i in InputNum:
+            flavor_property.SingleVmFlag[i] = 1
+        ToSerNum += 1
+    else:
+        judge_flag = 0
+print(server_property.cpu,server_property.flavor,server_property.memory,server_property.ser_name)        
+                
+                
+                
 
 
 
-     
+#
+#
+#     
