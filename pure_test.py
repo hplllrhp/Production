e69518ccbf881a904a7705dfafa01712f7f91af6
hplllrhp <hplllrhp@outlook.com>
2018-03-18 15:50:58 +0800

    备份

--- pure_test.py
+++ pure_test.py
@@ -8,7 +8,7 @@ Created on Sat Mar 17 21:34:19 2018
 #小电脑地址'E:\\coding\\python\\soft_game\\input_data.txt'
 #专门用来测试算法
 import os
-inputFilePath = 'E:\\coding\\python\\soft_game\\input_data.txt'
+inputFilePath = 'E:\\Document\\Personal\\Postgraduate\\game\\huawei_soft\\huawei\\ecs\\soft_game\\input_data.txt'
 def read_lines(file_path):
     if os.path.exists(file_path):
         array = []
@@ -27,13 +27,13 @@ class flavor_property:
         self.rest = []
         self.cpu = []      # property[0]:cpu
         self.memory = []
-        self.SingleVmFlag = 1   #放置开关
+        self.SingleVmFlag = []   #放置开关
 class server_property:
     def __init__(self):
         self.cpu = []     # property[0]:cpu
         self.memory = []
-        self.flavor = []        # flavor[0]表示要放的第1种虚拟机的台数，这里用下标直接代替了虚拟机种类，很明显可以flavor.resize(InputNum)
-
+        self.flavor = [[0 for i in range(5)] for i in range(1)]        # flavor[0]表示要放的第1种虚拟机的台数，这里用下标直接代替了虚拟机种类，很明显可以flavor.resize(InputNum)
+        self.ser_name = []
 
  
 def get_server_property(inputFilePath):
@@ -41,6 +41,7 @@ def get_server_property(inputFilePath):
     temp_list = server_file_array[0].split()
     server_property.cpu.append(int(temp_list[0]))
     server_property.memory.append(int(temp_list[1]))
+    server_property.ser_name.append('server')
     return server_property
 
 def get_flavor_property(inputFilePath):
@@ -52,6 +53,7 @@ def get_flavor_property(inputFilePath):
             flavor_property.name.append(temp_list[0])
             flavor_property.cpu.append(int(temp_list[1]))
             flavor_property.memory.append(int(int(temp_list[2])/1024))
+            flavor_property.SingleVmFlag.append(1)
     return flavor_property
 
 #print(server_property.property)
@@ -63,7 +65,6 @@ def JudgeRest(InputNum):
             if(server_property.cpu[0] >= flavor_property.cpu[i] and server_property.memory[0] >= flavor_property.memory[i]):
                flag = 1
                break
-    print(server_property.cpu,flavor_property.total[i])
     return flag
 
 def SelectFlavorToSet(InputNum,i):
@@ -73,25 +74,67 @@ def SelectFlavorToSet(InputNum,i):
     for j in range(InputNum):
         if(flavor_property.rest[j] != 0 and flavor_property.SingleVmFlag != 0):
             a.append(j)
+    print('a = ',a)
     size = len(a)
-    for k in range(size):
+    fn = a[0]
+    for k in range(1,size):
         if(i == 0):
-            if(flavor_property.cpu[a[k]] > flavor_property.cpu[a[k]-1]):
+            print('cpu',flavor_property.cpu[a[k]])
+            if(flavor_property.cpu[a[k]] > flavor_property.cpu[fn]):
                 fn = a[k]
-            elif(flavor_property.cpu[a[k]] < flavor_property.cpu[a[k]-1]):
-                fn = a[k-1]
+            elif(flavor_property.cpu[a[k]] < flavor_property.cpu[fn]):
+                fn = fn
             else:
-                fn = flavor_property.memory[a[k]] > flavor_property.memory[a[k -1]] ? a[k] : a[k-1] 
-    print()
-    
+                if(flavor_property.memory[a[k]] > flavor_property.memory[fn]):
+                    fn = a[k]
+                else:
+                    fn = fn
+            print('fn',fn)
+        else:
+            if(flavor_property.memory[a[k]] > flavor_property.memory[fn]):
+                fn = a[k]
+            elif(flavor_property.memory[a[k]] < flavor_property.memory[fn]):
+                fn = fn
+            else:
+                if(flavor_property.cpu[a[k]] > flavor_property.cpu[fn]):
+                    fn = a[k]
+                else:
+                    fn = fn
+    return fn
+
+def Judge(InputNum):
+    flag = 1
+    num = 0
+    for i in range(InputNum):
+        if(flavor_property.rest[i] == 0):
+            num += 1
+    if(num == InputNum):
+        flag = 0
+    return flag
+        
+            
+       
 #--------------------放置函数结束----------------------
 flavor_property = flavor_property()
 server_property = server_property()
 server_property = get_server_property(inputFilePath)
 flavor_property = get_flavor_property(inputFilePath)
 flavor_property.total = [2, 3, 3, 2, 6]#等于预测出来的数量之后用变量传递
-flavor_property.rest = [0,0,0,0,0]
-flag = JudgeRest(4)
+flavor_property.rest = [2, 3, 3, 2, 6]
+print('len',len(flavor_property.name))
+print(JudgeRest(5),SelectFlavorToSet(5,0),Judge(5)) 
+OptimizeFlag = 0
+server_property.ser_num = 0
+judge_flag = Judge(len(flavor_property.name))
+ToSerNum = 0;#//设置需要配置的服务器，ToSerNum = 0即第1个服务器
+fn = 0 #//当前放置优先级最高的虚拟机种类
+while (judge_flag):
+    flag = 1#//设置能否配置的标志位，因为是还未配置的虚拟机，所以一开始是可以配置的
+    server_property.flavor[ToSerNum][:] = 0
+    
+list2 = [[0 for i in range(5)] for i in range(2)]
+list2[:][0] = 1
+  
 
 
 
