cab63ecdf515e05284bfb19bae7b37293c53f36f
hplllrhp <hplllrhp@outlook.com>
2018-03-28 10:16:25 +0800

    备份

--- LSTM.py
+++ LSTM.py
@@ -20,23 +20,44 @@ def sigmoid_output_to_derivative(output):
 def matrixMul2(A, B):
     return [[sum(a * b for a, b in zip(a, b)) for b in zip(*B)] for a in A]
 
+def creat_matrix(x,y,start=0,step=1):  
+     N=[]  
+     F=[]  
+     for i in range(x):                  #等价于for(i=0,i<x,i++)  
+         for j in range(y):              
+             F.append(0)  
+#             start += step  
+         N.append(F)  
+         F=[]  
+     return N  
+ 
+def random_creat_matrix(x,y,start=0,step=1):  
+     N=[]  
+     F=[]  
+     for i in range(x):                  #等价于for(i=0,i<x,i++)  
+         for j in range(y):              
+             F.append(random.random())  
+#             start += step  
+         N.append(F)  
+         F=[]  
+     return N  
+
 int2binary = {}  
 binary_dim = 8 
-
 # input variables  
 alpha = 0.1  
 input_dim = 1 
-hidden_dim = 8  
+hidden_dim = 16  
 output_dim = 1 
 
 # initialize neural network weights  
-synapse_0 = random.random((input_dim,hidden_dim)) 
-synapse_1 = random.random((hidden_dim,output_dim)) 
-synapse_h = random.random((hidden_dim,hidden_dim))  
+synapse_0 = random_creat_matrix(input_dim,hidden_dim) 
+synapse_1 = random_creat_matrix(hidden_dim,output_dim) 
+synapse_h = random_creat_matrix(hidden_dim,hidden_dim)  
 
-synapse_0_update = zeros_like(synapse_0)  
-synapse_1_update = zeros_like(synapse_1)  
-synapse_h_update = zeros_like(synapse_h) 
+synapse_0_update = creat_matrix(len(synapse_0),len(synapse_0[0]))  
+synapse_1_update = creat_matrix(len(synapse_1),len(synapse_1[0]))  
+synapse_h_update = creat_matrix(len(synapse_h),len(synapse_h[0])) 
 
 
 
